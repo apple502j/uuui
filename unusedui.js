a0a4cb4617c9024916919b2e91780962a10617e1
apple502j <33279053+apple502j@users.noreply.github.com>
2018-01-28 18:57:42 +0900

    Create unusedui.js

---
+++ unusedui.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         UnUsedUI
+// @version      0.1
+// @description  UnusedUI Helper
+// @author       Apple502j and Kenny2scratch
+// @match        https://wiki.scratch.mit.edu/wiki/*
+// @match        https://wiki.scratch.mit.edu/w/*
+// ==/UserScript==
+
+// (C) 2018 Apple502j,Kenny2github All rights reserved
+
+function getQueryVariable(variable) {
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+function done(settings) {
+    var summs = settings.split(';;');
+    var lab = document.getElementById('mw-editpage-watch');
+    lab.innerHTML = '';
+
+    var label = document.createElement('label');
+    label.innerHTML = 'Watch Summary:';
+    label.for = 'wpSummary';
+    lab.appendChild(label);
+
+    summs.filter(function(i){
+        return i.trim() !== '';
+    }).forEach(function(i){
+        var summ = document.createElement('code');
+        summ.style.margin = '0.5em';
+        summ.style.borderRadius = '2px';
+        summ.style.border = '1px solid #d9dbe0';
+        summ.style.backgroundColor = 'f8f9fa';
+        summ.style.padding = '1px 4px';
+        summ.title = 'Insert canned edit summary';
+        summ.onclick = function(){
+            document.getElementById('wpTextbox1').value += "{{unusedui|year={{subst:REVISIONYEAR}}|month={{subst:REVISIONMONTH1}}|day={{subst:REVISIONDAY}}}}";
+            document.getElementById('wpSummary').value = this.innerHTML;
+            document.getElementById('wpSave').click();
+        };
+        summ.innerHTML = i;
+        lab.appendChild(summ);
+    });
+}
+
+if (['edit', 'submit'].includes(getQueryVariable('action')) && getQueryVariable('section') != 'new') {
+    summaries = "Unused User Image:put {{unusedui}}";
+    done(summaries);
+}
